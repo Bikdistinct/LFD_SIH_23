2cbb0ba8ffc3eb0e954055ed4bd39dd5691b660f
ritz <sritwick10@gmail.com>
2023-09-23 13:29:43 +0530

    middleware and routes setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.get('/', (req, res)=>{
-    res.send('Hello World');
+    res.send('MedTech');
 });
 
 app.listen(port, ()=> {
